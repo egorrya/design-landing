7b878cd4f9fba16a257292f963cff906b3d6a20c
Egor Rya <fayncore@gmail.com>
2022-07-11 15:42:50 +0300

    100vh

--- script.js
+++ script.js
@@ -204,10 +204,14 @@ if (popup) {
 document.addEventListener('DOMContentLoaded', footerAbsolute());
 
 // fix 100vh on mobile
-const appHeight = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty('--app-height', `${window.innerHeight}px`);
-};
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener('resize', appHeight());
-appHeight();
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
