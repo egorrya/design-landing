b92769b2ac84acf90ea5c009dbccf68d75a21cf0
Egor <fayncore@gmail.com>
2022-07-18 17:54:16 +0300

    build

--- script.js
+++ script.js
@@ -1,159 +1,231 @@
-const active = document.querySelector('.navigation__link--active'),
-  createActiveColorNav = (a, c) => {
-    let b = (a) =>
-      document.querySelectorAll(`.${c}`).forEach((b) => {
-        b.style.opacity = a;
-      });
-    a && 'navigation__link' === c
-      ? (b('0.3'), (a.style.opacity = '1'))
-      : b('1');
-  },
-  handleHover = (a, c, b) => {
-    if (a.target.classList.contains(b)) {
-      let d = a.target,
-        e = d.closest(`.${c}`).querySelectorAll(`.${b}`);
-      e.forEach((a) => {
-        a === d && (a.style.opacity = '1'),
-          a !== d && (a.style.opacity = '0.3');
-      });
-    }
-  },
-  nav = document.querySelector('.navigation');
-nav.classList.add('animation'),
-  nav.addEventListener('mouseover', (a) =>
-    handleHover(a, 'navigation', 'navigation__link')
-  ),
-  nav.addEventListener('mouseout', () =>
-    createActiveColorNav(active, 'navigation__link')
-  ),
-  createActiveColorNav(active, 'navigation__link');
-const footer = document.querySelector('.footer');
-footer.addEventListener('mouseover', (a) =>
-  handleHover(a, 'footer__list', 'footer__link')
-),
-  footer.addEventListener('mouseout', () =>
-    createActiveColorNav(active, 'footer__link')
-  ),
-  (document.querySelector('.footer__copyright--year').innerHTML =
-    new Date().getFullYear());
-const footerAbsolute = () => {
-  let b = document.querySelector('header').scrollHeight,
-    c = document.querySelector('main').scrollHeight;
-  document.querySelector('.footer').scrollHeight;
-  let a = document.querySelector('.footer');
-  window.innerHeight > c + b
-    ? (a.classList.add('footer__absolute'),
-      (document.querySelector('body').style.overflowY = 'hidden'))
-    : (a.classList.remove('footer__absolute'),
-      (document.querySelector('body').style.overflowY = 'auto'));
+// active color menu
+const active = document.querySelector('.navigation__link--active');
+
+const createActiveColorNav = (activeLink, className) => {
+  const makeAllOneOpacity = (opacityNumber) =>
+    document.querySelectorAll(`.${className}`).forEach((el) => {
+      el.style.opacity = opacityNumber;
+    });
+
+  if (activeLink && className === 'navigation__link') {
+    // one active
+    makeAllOneOpacity('0.3');
+    activeLink.style.opacity = '1';
+  } else {
+    // all active
+    makeAllOneOpacity('1');
+  }
 };
+
+// menu hover
+const handleHover = (e, parentClass, childClass) => {
+  if (e.target.classList.contains(childClass)) {
+    const link = e.target;
+    const siblings = link
+      .closest(`.${parentClass}`)
+      .querySelectorAll(`.${childClass}`);
+
+    siblings.forEach((el) => {
+      if (el === link) el.style.opacity = '1';
+      if (el !== link) el.style.opacity = '0.3';
+    });
+  }
+};
+
+// header nav opacity
+const nav = document.querySelector('.navigation');
+
+nav.classList.add('animation');
+nav.addEventListener('mouseover', (e) =>
+  handleHover(e, 'navigation', 'navigation__link')
+);
+nav.addEventListener('mouseout', () =>
+  createActiveColorNav(active, 'navigation__link')
+);
+createActiveColorNav(active, 'navigation__link');
+
+// footer nav opacity
+const footer = document.querySelector('.footer');
+footer.addEventListener('mouseover', (e) =>
+  handleHover(e, 'footer__list', 'footer__link')
+);
+footer.addEventListener('mouseout', () =>
+  createActiveColorNav(active, 'footer__link')
+);
+
+// current year in the footer
+document.querySelector('.footer__copyright--year').innerHTML =
+  new Date().getFullYear();
+
+// add absolute to the footer
+const footerAbsolute = () => {
+  const headerHeight = document.querySelector('header').scrollHeight;
+  const mainHeight = document.querySelector('main').scrollHeight;
+  const footerHeight = document.querySelector('.footer').scrollHeight;
+  const footer = document.querySelector('.footer');
+
+  if (window.innerHeight > mainHeight + headerHeight) {
+    footer.classList.add('footer__absolute');
+    document.querySelector('body').style.overflowY = 'hidden';
+  } else {
+    footer.classList.remove('footer__absolute');
+    document.querySelector('body').style.overflowY = 'auto';
+  }
+};
+
+// add fade in to every project
 document.addEventListener('DOMContentLoaded', () => {
-  let a = document.querySelector('.projects--one');
-  a &&
-    a.querySelectorAll('.projects__item').forEach((b, a) => {
-      setTimeout(() => {
-        b.classList.add('fade-in');
-      }, 100 * a);
-    });
-  let b = document.querySelector('.projects__inside');
-  b &&
-    b.querySelectorAll('.projects__item').forEach((b, a) => {
-      setTimeout(() => {
-        b.classList.add('fade-in');
-      }, 100 * a);
-    });
+  const projectsOneSection = document.querySelector('.projects--one');
+
+  if (projectsOneSection) {
+    projectsOneSection
+      .querySelectorAll('.projects__item')
+      .forEach((item, index) => {
+        setTimeout(() => {
+          item.classList.add('fade-in');
+        }, index * 100);
+      });
+  }
+
+  const projectsInside = document.querySelector('.projects__inside');
+
+  if (projectsInside) {
+    projectsInside
+      .querySelectorAll('.projects__item')
+      .forEach((item, index) => {
+        setTimeout(() => {
+          item.classList.add('fade-in');
+        }, index * 100);
+      });
+  }
 });
-const popup = document.querySelector('.popup'),
-  projectOpenPopup = document.querySelector('.project__main-image.video'),
-  homeButton = document.querySelector('.home__button'),
-  fullscreenElement =
-    document.fullscreenElement ||
-    document.mozFullScreenElement ||
-    document.webkitFullscreenElement ||
-    document.msFullscreenElement;
+
+const popup = document.querySelector('.popup');
+const projectOpenPopup = document.querySelector('.project__main-image.video');
+const homeButton = document.querySelector('.home__button');
+const fullscreenElement =
+  document.fullscreenElement ||
+  document.mozFullScreenElement ||
+  document.webkitFullscreenElement ||
+  document.msFullscreenElement;
+
+// popup
 function openPopupVideo() {
-  let a = popup.querySelector('video');
-  window.scrollTo(0, 0),
-    document.querySelector('body').classList.add('overflow-y-none'),
-    popup.classList.add('popup--show'),
-    (a.style.display = 'block'),
-    a.paused && a.play(),
-    (a.muted = !1),
-    (a.volume = 0.5),
-    (a.currentTime = 0);
+  const popupVideo = popup.querySelector('video');
+
+  window.scrollTo(0, 0);
+  document.querySelector('body').classList.add('overflow-y-none');
+  popup.classList.add('popup--show');
+  popupVideo.style.display = 'block';
+
+  if (popupVideo.paused) popupVideo.play();
+  popupVideo.muted = false;
+  popupVideo.volume = 0.5;
+  popupVideo.currentTime = 0;
 }
-if (projectOpenPopup && popup && window.innerWidth >= 680) {
-  let a = projectOpenPopup.querySelector('video');
-  a.muted || (a.muted = !0),
-    projectOpenPopup.addEventListener('click', () => {
-      openPopupVideo();
-    });
-} else
-  projectOpenPopup &&
-    popup &&
-    window.innerWidth <= 680 &&
-    (popup.remove(), projectOpenPopup.querySelector('video').remove());
-homeButton &&
-  popup &&
+
+if (projectOpenPopup && popup && window.innerWidth >= 320) {
+  const videoInsideHeader = projectOpenPopup.querySelector('video');
+  if (!videoInsideHeader.muted) videoInsideHeader.muted = true;
+
+  projectOpenPopup.addEventListener('click', () => {
+    openPopupVideo();
+  });
+} else if (projectOpenPopup && popup && window.innerWidth <= 320) {
+  popup.remove();
+  projectOpenPopup.querySelector('video').remove();
+}
+
+if (homeButton && popup)
   homeButton.addEventListener('click', () => {
     openPopupVideo();
-  }),
-  popup &&
-    popup.addEventListener('click', () => {
-      let a = popup.querySelector('video');
-      document.querySelector('body').classList.remove('overflow-y-none'),
-        popup.classList.remove('popup--show'),
-        (a.muted = !0),
-        (a.style.display = 'none'),
-        projectOpenPopup &&
-          (projectOpenPopup.querySelector('video').play(),
-          (projectOpenPopup.querySelector('video').muted = !0));
-    }),
-  document
-    .querySelector('.navigation__button')
-    .addEventListener('click', () => {
-      window.scrollTo(0, 0),
-        document.querySelector('body').classList.toggle('overflow-y-none');
-    });
-const loadMore = document.querySelector('.load-more'),
-  projectsTwoSection = document.querySelector('.projects--two');
-if (
-  (loadMore &&
-    projectsTwoSection &&
-    ((loadMore.style.display = 'flex'),
-    (projectsTwoSection.style.display = 'none'),
+  });
+
+if (popup)
+  popup.addEventListener('click', () => {
+    const popupVideo = popup.querySelector('video');
+
+    document.querySelector('body').classList.remove('overflow-y-none');
+    popup.classList.remove('popup--show');
+
+    popupVideo.muted = true;
+    popupVideo.style.display = 'none';
+
+    if (projectOpenPopup) {
+      projectOpenPopup.querySelector('video').play();
+      projectOpenPopup.querySelector('video').muted = true;
+    }
+  });
+
+// mobile nav
+document.querySelector('.navigation__button').addEventListener('click', () => {
+  window.scrollTo(0, 0);
+  document.querySelector('body').classList.toggle('overflow-y-none');
+});
+
+// load more
+const loadMore = document.querySelector('.load-more');
+const projectsTwoSection = document.querySelector('.projects--two');
+
+if (loadMore && projectsTwoSection) {
+  // load more button init
+  loadMore.style.display = 'flex';
+  projectsTwoSection.style.display = 'none';
+
+  // fade in init
+  projectsTwoSection
+    .querySelectorAll('.projects__item')
+    .forEach((item) => item.classList.add('opacity-zero'));
+
+  loadMore.addEventListener('click', () => {
+    // hide button
+    loadMore.style.opacity = '0';
+    loadMore.style.position = 'absolute';
+    // loadMore.querySelector('a').style.fontSize = '0';
+
+    // show projects
+    projectsTwoSection.style.display = 'flex';
+
     projectsTwoSection
       .querySelectorAll('.projects__item')
-      .forEach((a) => a.classList.add('opacity-zero')),
-    loadMore.addEventListener('click', () => {
-      (loadMore.style.opacity = '0'),
-        (loadMore.style.position = 'absolute'),
-        (projectsTwoSection.style.display = 'flex'),
-        projectsTwoSection
-          .querySelectorAll('.projects__item')
-          .forEach((b, a) => {
-            setTimeout(() => {
-              b.classList.add('fade-in');
-            }, 100 * a);
-          }),
-        footerAbsolute();
-    })),
-  popup)
-) {
-  let b,
-    c = () => {
-      popup.style.cursor = 'none';
-    };
-  popup.addEventListener('mousemove', () => {
-    (popup.style.cursor = "url('./img/close.png'), auto"),
-      clearTimeout(b),
-      (b = setTimeout(c, 3e3));
+      .forEach((item, index) => {
+        setTimeout(() => {
+          item.classList.add('fade-in');
+        }, index * 100);
+      });
+
+    footerAbsolute();
   });
 }
-document.addEventListener('DOMContentLoaded', footerAbsolute());
-let vh = 0.01 * window.innerHeight;
-document.documentElement.style.setProperty('--vh', `${vh}px`),
-  window.addEventListener('resize', () => {
-    let a = 0.01 * window.innerHeight;
-    document.documentElement.style.setProperty('--vh', `${a}px`);
+
+// mouse moving inside popup cursor
+if (popup) {
+  let timer;
+
+  const mouseStopped = () => {
+    // the actual function that is called
+    popup.style.cursor = 'none';
+  };
+
+  popup.addEventListener('mousemove', () => {
+    popup.style.cursor = "url('./img/close.png'), auto";
+    clearTimeout(timer);
+    timer = setTimeout(mouseStopped, 3000);
   });
+}
+
+// footer absolute
+document.addEventListener('DOMContentLoaded', footerAbsolute());
+
+// fix 100vh on mobile
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
